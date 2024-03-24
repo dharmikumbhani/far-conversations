a853540ff2857197b57b25d3cf050fa418e4af85
dharmikumbhani <dharmikumbhani201@gmail.com>
2024-03-24 21:37:35 +0530

    Reverted AI Response

--- app/lib/gptAI.ts
+++ app/lib/gptAI.ts
@@ -1,46 +1,46 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const apiKey = process.env.OPENAI_API_KEY;
-// const apiUrl = 'https://api.openai.com/v1/completions';
-
-// export async function getAIResponse(promptText: any) {
-//   try {
-//     const response = await axios.post(
-//         'https://api.openai.com/v1/completions',
-//         {
-//             'model': 'text-davinci-003',
-//             'prompt': promptText,
-//             'temperature': 0.7
-//         },
-//         {
-//             headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': 'Bearer process.env.OPENAI_API_KEY'
-//             }
-//         }
-//         );
-//     if (response.status === 200) {
-//         console.log('Response', response);
-//         return response.data.choices[0].text.trim();
-//     } else {
-//       return `Error: ${response.status} - ${response.statusText}`;
-//     }
-//   } catch (error) {
-//     return `Error: ${error}`;
-//   }
-// }
-
-import OpenAI from 'openai';
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const apiKey = process.env.OPENAI_API_KEY;
+const apiUrl = 'https://api.openai.com/v1/completions';
 
 export async function getAIResponse(promptText: any) {
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: 'Say this is a test' }],
-    model: 'gpt-3.5-turbo',
-  });
-  console.log(chatCompletion)
-  return chatCompletion;
+  try {
+    const response = await axios.post(
+        'https://api.openai.com/v1/completions',
+        {
+            'model': 'text-davinci-003',
+            'prompt': promptText,
+            'temperature': 0.7
+        },
+        {
+            headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer process.env.OPENAI_API_KEY'
+            }
+        }
+        );
+    if (response.status === 200) {
+        console.log('Response', response);
+        return response.data.choices[0].text.trim();
+    } else {
+      return `Error: ${response.status} - ${response.statusText}`;
+    }
+  } catch (error) {
+    return `Error: ${error}`;
+  }
 }
+
+// import OpenAI from 'openai';
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// export async function getAIResponse(promptText: any) {
+//   const chatCompletion = await openai.chat.completions.create({
+//     messages: [{ role: 'user', content: promptText }],
+//     model: 'gpt-3.5-turbo',
+//   });
+//   console.log(chatCompletion)
+//   return chatCompletion;
+// }
